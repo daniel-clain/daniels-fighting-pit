7a4d58e789bd9a7a8f8bb762d45b31ec229ed931
Daniel Clain <daniel.clain@hotmail.com>
2019-03-29 09:09:10 +0000

    delete js

--- src/enums/fightStates.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var FightStates;
-(function (FightStates) {
-    FightStates["beginning of fight"] = "beginning of fight";
-    FightStates["active"] = "active";
-    FightStates["end of fight"] = "end of fight";
-})(FightStates = exports.FightStates || (exports.FightStates = {}));
--- src/enums/roundStates.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var RoundStates;
-(function (RoundStates) {
-    RoundStates["pre fight"] = "pre fight";
-    RoundStates["news headlines"] = "news headlines";
-    RoundStates["fight day"] = "fight day";
-    RoundStates["end of round"] = "end of round";
-})(RoundStates = exports.RoundStates || (exports.RoundStates = {}));
